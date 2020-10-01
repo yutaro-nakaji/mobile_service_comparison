43975620a2767c12dfd00b081f56ac12b09da4d9
yutaro-nakaji <quadcore.4412gmail.com>
2020-10-01 10:38:23 +0900

    通話方式選択ボタン実装

--- app/models/data_traffic.rb
+++ app/models/data_traffic.rb
@@ -1,6 +1,6 @@
 class DataTraffic < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },
+    { id: 1, name: '選択してください' },
     { id: 2, name: '〜1GB' },
     { id: 3, name: '〜2GB' },
     { id: 4, name: '〜3GB' },
