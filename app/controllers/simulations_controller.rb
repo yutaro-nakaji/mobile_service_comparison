9f33951197142c1c27f7d35bdf6fee48e427f34b
yutaro-nakaji <quadcore.4412gmail.com>
2020-10-07 13:48:01 +0900

    コントローラー作成

--- app/controllers/simulations_controller.rb
+++ app/controllers/simulations_controller.rb
@@ -1,10 +1,24 @@
 class SimulationsController < ApplicationController
+  
   def index
   end
+  
   def new
+    @simulation = Simulation.new
   end
+  
+  def create
+    @simulation = Simulation.new(simulation_params)
+    if @simulation.save
+      redirect_to :show
+    else
+      render :new
+    end
+  end
+  
   def edit
   end
+  
   def show
   end
 
