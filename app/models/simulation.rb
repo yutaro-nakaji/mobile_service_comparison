cb7348ebcd21d2eca563458224b6ef88f1de69db
yutaro-nakaji <quadcore.4412gmail.com>
2020-10-08 22:20:14 +0900

    ラジオボタンバグ修正

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -1,11 +1,11 @@
 class Simulation < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :current_phone
-  belongs_to_active_hash :calling_plan
+  #belongs_to_active_hash :current_phone
+  #belongs_to_active_hash :calling_plan
   belongs_to_active_hash :data_traffic
   belongs_to_active_hash :generation
-  belongs_to_active_hash :number
+  #belongs_to_active_hash :number
   belongs_to_active_hash :optical_line
 
 end
